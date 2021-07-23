743c8c58dc19d2f247aa2361630afc4a27ee907c
dalakoti07 <saurabhdalakoti07@gmail.com>
2021-07-23 11:16:23 +0530

    added mockito implementation

--- 07-introduction-to-mockito/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/GameUnitTests.kt
+++ 07-introduction-to-mockito/projects/starter/app/src/test/java/com/raywenderlich/android/cocktails/game/model/GameUnitTests.kt
@@ -30,37 +30,12 @@
 
 package com.raywenderlich.android.cocktails.game.model
 
+import com.nhaarman.mockitokotlin2.*
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
 
 class GameUnitTests {
-
-  @Test
-  fun whenIncrementingScore_shouldIncrementCurrentScore() {
-    val game = Game(emptyList(), 0)
-
-    game.incrementScore()
-
-    Assert.assertEquals("Current score should have been 1", 1, game.currentScore)
-  }
-
-  @Test
-  fun whenIncrementingScore_aboveHighScore_shouldAlsoIncrementHighScore() {
-    val game = Game(emptyList(), 0)
-
-    game.incrementScore()
-
-    Assert.assertEquals(1, game.highestScore)
-  }
-
-  @Test
-  fun whenIncrementingScore_belowHighScore_shouldNotIncrementHighScore() {
-    val game = Game(emptyList(), 10)
-
-    game.incrementScore()
-
-    Assert.assertEquals(10, game.highestScore)
-  }
 
   @Test
   fun whenGettingNextQuestion_shouldReturnIt() {
@@ -84,4 +59,40 @@ class GameUnitTests {
 
     Assert.assertNull(nextQuestion)
   }
+
+  @Test
+  fun whenAnswering_shouldDelegateToQuestion() {
+    val question = mock<Question>()
+    val game = Game(listOf(question))
+
+    game.answer(question, "OPTION")
+
+    verify(question, times(1)).answer(eq("OPTION"))
+  }
+
+  @Test
+  fun whenAnsweringCorrectly_shouldIncrementCurrentScore() {
+    val question = mock<Question>()
+    whenever(question.answer(anyString())).thenReturn(true)
+    val score = mock<Score>()
+    val game = Game(listOf(question), score)
+
+    game.answer(question, "OPTION")
+
+    verify(score).increment()
+  }
+
+  @Test
+  fun whenAnsweringIncorrectly_shouldNotIncrementCurrentScore() {
+    val question = mock<Question>()
+    whenever(question.answer(anyString())).thenReturn(false)
+    val score = mock<Score>()
+    val game = Game(listOf(question), score)
+
+    game.answer(question, "OPTION")
+
+    verify(score, never()).increment()
+  }
+
+
 }
